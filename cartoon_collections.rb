6c3339560b7747d7486ca6d067890f7e72db2a50
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-19 18:22:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+require 'pp'
+# # --This is my non-enumerator code--
+# def roll_call_dwarves(dwarf_names)
+#   i = 0
+#   while i < dwarf_names.length
+#     puts "#{i + 1}. #{dwarf_names[i]}"
+#     i += 1
+#   end
+# end
+
+def roll_call_dwarves(dwarf_names)
+  new_array = []
+  i = 1
+  dwarf_names.each do |dwarf|
+    new_array << "#{i}. #{dwarf}"
+    i += 1
+  end
+  puts new_array
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet
   # Your code here
 end
 
